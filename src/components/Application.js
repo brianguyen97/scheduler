1ed195bbf69c35d8e1c90eb9942db05451428ba1
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-07-12 00:48:47 -0600

    Finish "Building a Button" activity

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import "components/Application.scss";
+import 'components/Application.scss';
 
 export default function Application(props) {
   return (
