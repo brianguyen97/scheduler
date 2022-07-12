eebaa4d81688e1e04f22461a73d489d6b61383a4
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-07-12 02:59:47 -0600

    Destructure props

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,18 +3,15 @@ import 'components/Button.scss';
 import classNames from 'classnames';
 
 export default function Button(props) {
+  const { onClick, disabled, children } = props;
   const buttonClass = classNames('button', {
     'button--confirm': props.confirm,
     'button--danger': props.danger,
   });
 
   return (
-    <button
-      className={buttonClass}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
+    <button className={buttonClass} onClick={onClick} disabled={disabled}>
+      {children}
     </button>
   );
 }
