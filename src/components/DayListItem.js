6b516e6e1a8b7c37b57ce16b777230406833c94a
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-07-12 16:46:37 -0600

    Style Buttons based on props passed

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './DayListItem.scss';
+import classNames from 'classnames';
 
 export default function DayListItem(props) {
-  const { name, spots, setDay } = props;
+  // const { name, spots, setDay, selected } = props;
+  const dayListClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0,
+  });
   return (
-    <li onClick={() => setDay(name)}>
-      <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+    <li className={dayListClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
