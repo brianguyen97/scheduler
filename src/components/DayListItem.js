91e268d86ebc00d182775edc3a826e766bc83bfe
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-07-16 02:50:55 -0600

    Refactor Code

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,16 +4,12 @@ import classNames from 'classnames';
 
 export default function DayListItem(props) {
   // const { name, spots, setDay, selected } = props;
-  const dayListClass = classNames('day-list__item', {
+  const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0,
   });
 
-  const setDay = name => {
-    console.log(name);
-  };
-
-  const renderSpots = spots => {
+  const formatSpots = spots => {
     if (spots === 0) return <h3 className="text--light">no spots remaining</h3>;
     if (spots === 1) return <h3 className="text--light">1 spot remaining</h3>;
     if (spots >= 2)
@@ -21,9 +17,13 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayListClass} onClick={() => setDay(props.name)}>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      {renderSpots(props.spots)}
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,14 +3,14 @@ import './InterviewerListItem.scss';
 import classNames from 'classnames';
 
 export default function InterviewerListItem(props) {
-  const { id, avatar, name, selected, setInterviewer } = props;
+  const { avatar, name, selected, setInterviewer } = props;
 
-  const listItemClasses = classNames('interviewers__item', {
+  const interviewerClass = classNames('interviewers__item', {
     'interviewers__item--selected': selected,
   });
 
   return (
-    <li key={id} className={listItemClasses} onClick={setInterviewer}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img className="interviewers__item-image" src={avatar} alt={name} />
       {selected && name}
     </li>
