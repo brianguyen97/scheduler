050dc8fe68064b28894eef6e681fe1bb47639361
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-07-21 05:49:05 -0600

    remove hard coded days array, use api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
+import axios from 'axios';
 
 const appointments = {
   1: {
@@ -65,7 +48,15 @@ const appointmentsData = Object.values(appointments).map(app => {
 });
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+
+  useEffect(() => {
+    axios.get('/api/days').then(result => {
+      setDays(result.data);
+    });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
