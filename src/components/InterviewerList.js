6de0af8968401bcea9271489fcb7b3e3a132950f
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-08-05 16:47:09 -0600

    Import PropTypes to InterviewerList.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem.js';
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const interviewersData = props.interviewers.map(interviewer => (
@@ -12,7 +13,6 @@ export default function InterviewerList(props) {
       setInterviewer={() => props.onClick(interviewer.id)}
     />
   ));
-
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -20,3 +20,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
