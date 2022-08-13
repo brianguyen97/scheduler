83660a7f5e31a700706ae85b7470c9617d13355a
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-08-13 10:15:13 -0600

    Axios mock

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -55,7 +55,7 @@ const fixtures = {
 
 export default {
   get: jest.fn(url => {
-    if (url === '/api/days') {
+    if (url === 'http://localhost:8001/api/days') {
       return Promise.resolve({
         status: 200,
         statusText: 'OK',
@@ -63,21 +63,41 @@ export default {
       });
     }
 
-    if (url === '/api/appointments') {
-      /* Resolve appointments data */
+    if (url === 'http://localhost:8001/api/appointments') {
       return Promise.resolve({
         status: 200,
-        statusText: 'OK',
+        statusText: 'Ok',
         data: fixtures.appointments,
       });
     }
 
-    if (url === '/api/interviewers') {
-      /* Resolve interviewers data */
+    if (url === 'http://localhost:8001/api/interviewers') {
       return Promise.resolve({
         status: 200,
         statusText: 'OK',
         data: fixtures.interviewers,
+      });
+    }
+  }),
+  put: jest.fn(url => {
+    if (url === 'http://localhost:8001/api/appointments/1') {
+      return Promise.resolve({
+        status: 204,
+        statusText: 'No Content',
+      });
+    }
+    if (url === 'http://localhost:8001/api/appointments/2') {
+      return Promise.resolve({
+        status: 204,
+        statusText: 'No Content',
+      });
+    }
+  }),
+  delete: jest.fn(url => {
+    if (url === 'http://localhost:8001/api/appointments/2') {
+      return Promise.resolve({
+        status: 204,
+        statusText: 'No content',
       });
     }
   }),
