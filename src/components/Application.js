a78b88052d94a7613145dca2c84d6ba4993bce6c
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-08-13 10:14:40 -0600

    Add time prop

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,7 +7,7 @@ import {
   getInterview,
   getInterviewersForDay,
 } from 'helpers/selectors';
-import useApplicationData from 'hooks/useApplicationData';
+import { useApplicationData } from 'hooks/useApplicationData';
 
 export default function Application(props) {
   const { state, setDay, bookInterview, cancelInterview } =
@@ -25,6 +25,7 @@ export default function Application(props) {
         id={appointment.id}
         interviewers={interviewersArray}
         interview={interview}
+        time={appointment.time}
         bookInterview={bookInterview}
         cancelInterview={cancelInterview}
       />
